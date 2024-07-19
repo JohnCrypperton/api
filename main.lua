b2f713e915dec87d13a533ec022e08784e8f2b0e
John Eleanor <82526875+JohnEleanor@users.noreply.github.com>
2024-07-19 14:09:28 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,22 @@
 [[
-onPlayerJoined = function()
-    print("load")
-    RegisterNetEvent("esx:onPlayerJoined")
-    AddEventHandler("esx:onPlayerJoined", function()
-        local _source = source
-        while not next(ESX.Jobs) do
-            Wait(50)
-        end
+local oneSyncState = GetConvar("onesync", "off")
+local newPlayer = "INSERT INTO `users` SET `accounts` = ?, `identifier` = ?, `group` = ?"
+local loadPlayer = "SELECT `accounts`, `job`, `job_grade`, `group`, `position`, `inventory`, `skin`, `loadout`, `metadata`"
 
-        if not ESX.Players[_source] then
-            onPlayerJoined(_source)
-        end
-    end)
+if Config.Multichar then
+    newPlayer = newPlayer .. ", `firstname` = ?, `lastname` = ?, `dateofbirth` = ?, `sex` = ?, `height` = ?"
 end
+
+if Config.StartingInventoryItems then
+    newPlayer = newPlayer .. ", `inventory` = ?"
+end
+
+if Config.Multichar or Config.Identity then
+    loadPlayer = loadPlayer .. ", `firstname`, `lastname`, `dateofbirth`, `sex`, `height`"
+end
+
+loadPlayer = loadPlayer .. " FROM `users` WHERE identifier = ?"
+
     
 ]]
 
